9185a2062fada492f7f54f4bcc6afb89bc499b3b
Pascalis Maschke <Pascalis@DEV-054.local>
2019-01-17 18:41:03 +0100

    pls review

--- Tutorium10/Pascalis/file.js
+++ Tutorium10/Pascalis/file.js
@@ -1,77 +1,89 @@
-    // 1
-    const getCharacterCount = (text) => {
-        return text ?
-            text.match(/./g).length : 0
-    }
+    // // 1
+    // const getCharacterCount = (text) => {
+    //     return text ?
+    //         text.match(/./g).length : 0
+    // }
 
-    console.log("getCharacterCount", getCharacterCount("!W-o r-t?"))
+    // console.log("getCharacterCount", getCharacterCount("!W-o r-t?"))
 
-    // 2
-    const getWords = (text) => {
-        const pattern = /((\w|€)+)/g
-        const words = text ? text.match(pattern) : []
-        return words
-    }
+    // // 2
+    // const getWords = (text) => {
+    //     const pattern = /((\w|€)+)/g
+    //     const words = text ? text.match(pattern) : []
+    //     return words
+    // }
 
-    console.log("getWords", getWords("Hi 123 _ 10€ 3.33"))
+    // console.log("getWords", getWords("Hi 123 _ 10€ 3.33"))
 
-    // 3
-    const getLongestWords = (text) => {
-        let maxLength = 0
-        const words = getWords(text)
-        words.forEach(x => maxLength = getCharacterCount(x) > maxLength ? getCharacterCount(x) : maxLength)
-        return words.filter(x => getCharacterCount(x) === maxLength)
-    }
+    // // 3
+    // const getLongestWords = (text) => {
+    //     let maxLength = 0
+    //     const words = getWords(text)
+    //     words.forEach(x => maxLength = getCharacterCount(x) > maxLength ? getCharacterCount(x) : maxLength)
+    //     return words.filter(x => getCharacterCount(x) === maxLength)
+    // }
 
-    console.log("getLongestWords", getLongestWords("Hi 123 _ 10€ 3.33"))
+    // console.log("getLongestWords", getLongestWords("Hi 123 _ 10€ 3.33"))
 
-    // 4 basically the solution to homework 7.4
-    const getWordFrequency = (text) => {
-        const words = getWords(text)
-        const map = words.map(x => x.toLowerCase()).reduce(
-            (frequency, y) =>
-            frequency.has(y) ? frequency.set(y, frequency.get(y) + 1) : frequency.set(y, 1),
-            new Map()
-        )
-        return text ? strMapToObj(map) : {}
-    }
+    // // 4 basically the solution to homework 7.4
+    // const getWordFrequency = (text) => {
+    //     const words = getWords(text)
+    //     const map = words.map(x => x.toLowerCase()).reduce(
+    //         (frequency, y) =>
+    //         frequency.has(y) ? frequency.set(y, frequency.get(y) + 1) : frequency.set(y, 1),
+    //         new Map()
+    //     )
+    //     return text ? strMapToObj(map) : {}
+    // }
 
-    const strMapToObj = strMap => {
-        const obj = Object.create(null)
-        for (const [k, v] of strMap) {
-            obj[k] = v
-        }
-        console.log(obj)
-        return obj
-    }
+    // const strMapToObj = strMap => {
+    //     const obj = Object.create(null)
+    //     for (const [k, v] of strMap) {
+    //         obj[k] = v
+    //     }
+    //     console.log(obj)
+    //     return obj
+    // }
 
-    console.log("getWordFrequency", getWordFrequency("Hi hi constructor _ 10€ 3.10€"))
+    // console.log("getWordFrequency", getWordFrequency("Hi hi constructor _ 10€ 3.10€"))
 
-    // 5
-    const filterTUBMails = (text) => {
-        words = text.match(/([a-zA-Z0-9._-]+@(mailbox.tu-berlin.de))/gi);
-        return words ? words : {}
-    }
+    // // 5
+    // const filterTUBMails = (text) => {
+    //     words = text.match(/([a-zA-Z0-9._-]+@(mailbox.tu-berlin.de))/gi);
+    //     return words ? words : []
+    // }
 
-    console.log("filterTUBMails", filterTUBMails("Eine mögliche Adresse ist max-mustermann@Mailbox.Tu-berlin.de aber auch admin+superschlau@mailbox.tu-berlin.de und auch pascalis.maschke@t-online.de."));
+    // console.log("filterTUBMails", filterTUBMails("Eine mögliche Adresse ist max-mustermann@Mailbox.Tu-berlin.de aber auch admin+superschlau@mailbox.tu-berlin.de und auch pascalis.maschke@t-online.de."));
 
     //6 
     const getUnpublishingDate = (text) => {
+
         const pattern = /([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])(T)([0-9]{2}):([0-9]{2}):([0-9]{2}).([0-9]{3})([A-Z])/
         const parts = text.match(pattern)
-        earliestDateOfDeletion = getEarliestDateOfDeletion()
-        console.log("earliestDateOfDeletion", earliestDateOfDeletion)
-
+        const earliestDateOfDeletion = getEarliestDateOfDeletion()
+        const maxDateOfDeletion = getMaxDateOfDeletion()
+            // console.log("earliestDateOfDeletion", earliestDateOfDeletion)
         const textToDate = new Date(text.replace(pattern, validateDate))
-        return textToDate
+            // if (textToDate instanceof Date) {
+            //     return earliestDateOfDeletion
+            // } else 
+        if (+textToDate < +maxDateOfDeletion && +textToDate > +earliestDateOfDeletion) {
+            return textToDate
+        } else if (text === "max" || +textToDate > +maxDateOfDeletion) {
+            return maxDateOfDeletion
+        } else if (text === "min") {
+            return earliestDateOfDeletion
+        }
+
+        return earliestDateOfDeletion
     }
     const validateDate = (match, year, month, day, time, hour, minute, second, millisecond, zone) => {
-        console.log("validateDate:", match, year, month, day, time, hour, minute, second, millisecond, zone)
+        // console.log("validateDate:", match, year, month, day, time, hour, minute, second, millisecond, zone)
         const newMinute = '00'
         const newSecond = '00'
         const newMillisecond = '000'
         const newTimeStamp = `${year}-${month}-${day}${time}${hour}:${newMinute}:${newSecond}.${newMillisecond}${zone}`
-        console.log("newTimeStamp", newTimeStamp)
+            // console.log("newTimeStamp", newTimeStamp)
         return newTimeStamp
     }
     const getEarliestDateOfDeletion = () => {
@@ -83,7 +95,21 @@
 
         return earliestDateOfDeletion
     }
+    const getMaxDateOfDeletion = () => {
+        const rightNow = new Date()
+        const maxDateOfDeletion = new Date(rightNow.setDate(rightNow.getDate() + 30))
+        maxDateOfDeletion.setSeconds(0)
+        maxDateOfDeletion.setMilliseconds(0)
+        maxDateOfDeletion.setMinutes(0)
 
-    console.log("getUnpublishingDate", getUnpublishingDate("2018-01-10T18:01:01.001Z"))
+        return maxDateOfDeletion
+    }
+
+    console.log("getUnpublishingDate 1", getUnpublishingDate("hiiiii i i i 001Z"))
+    console.log("getUnpublishingDate", getUnpublishingDate("2019:01-20T18:01:01.001Z"))
+    console.log("getUnpublishingDate afterMax", getUnpublishingDate("2019-05-10T18:01:01.001Z"))
+    console.log("getUnpublishingDate max", getUnpublishingDate("max"))
+    console.log("getUnpublishingDate min", getUnpublishingDate("min"))
+    console.log("getUnpublishingDate ", getUnpublishingDate(""))
 
     //"$1 $2 $3 $4 $5 $6 $7 $8 $9"
